f7d22ffe935a27366ef74e6775f6c9a631b3fa30
Steffen Leppin <leppinsteffen@gmail.com>
2021-05-13 19:43:55 +0200

    sidebar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import translator from '../services/translate'
+import SideBar from './SideBar'
 
-export default function Home() {
+class Home extends React.Component {
+  state = {
+    sideBar: false,
+    wordToBeTranslated: ''
+  }
 
-  return (
-    <main>
-      <h1>Headline</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est assumenda modi dolore ab doloremque non consectetur mollitia. Illum perspiciatis voluptate hic necessitatibus vel consequuntur sint voluptates nisi libero iste eius quas ipsam quasi in placeat esse, cum pariatur! Ipsam ipsa blanditiis numquam! Rem, necessitatibus aliquam. Unde suscipit mollitia repellendus culpa.</p>
-    </main>
-  )
+  showSideBar = (e) => {
+    this.setState({
+      sideBar: !this.state.sideBar,
+      wordToBeTranslated: e.target.innerText
+    })
+  }
+
+  render() {
+    return (
+      <main>
+        <h1 onClick={ e => this.showSideBar(e) } > Headline</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, ducimus?</p>
+        <button onClick={ e => this.showSideBar(e) }>show SideBar</button>
+        { this.state.sideBar && <SideBar wordToBeTranslated={ this.state.wordToBeTranslated } /> }
+      </main >
+    )
+  }
 }
+
+export default Home;
---
+++ client/src/components/SideBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class SideBar extends React.Component {
+  state = {
+    showVocabList: false,
+  }
+
+  render() {
+    console.log(this.props);
+
+    return (
+      <aside>
+        <div className="translation">
+          <h3>{ this.props.wordToBeTranslated}</h3>
+          <p>definition</p>
+        </div>
+
+        <button onClick={ e => this.setState({ showVocabList: !this.state.showVocabList }) }>add to vocabulary list</button>
+
+        <button>Already Read</button>
+        {this.state.showVocabList && <h3>your lists</h3> }
+      </aside>
+    )
+  }
+}
+
+export default SideBar;
