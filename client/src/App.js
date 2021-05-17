9b9572eb61e52c19328d638d67305a950142dbc7
Andy <andrew.mcginn@gmail.com>
2021-05-17 13:54:43 +0100

    flash cards created

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Dashboard from './components/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 import LibraryPage from './components/LibraryPage';
 import SpecificText from './components/SpecificText';
+import FlashCardGame from './components/FlashCardGame';
 import NotFound from './components/NotFound'
 
 
@@ -41,6 +42,7 @@ class App extends React.Component {
             <ProtectedRoute path='/dashboard' user={ this.state.user } component={ Dashboard } redirectPath="/" />
             <Route exact path='/library' render={ props => <LibraryPage user={ this.state.user } setUser={ this.setUser } { ...props } /> } />
             <Route exact path='/texts/:id' render={ props => <SpecificText user={ this.state.user } { ...props } /> } />
+            <Route exact path='/vocablists/:id/flashcards' render={ props => <FlashCardGame user={ this.state.user } { ...props } /> } />
             <Route path="/404" component={ NotFound } />
             <Redirect to="/404" />
           </Switch>
