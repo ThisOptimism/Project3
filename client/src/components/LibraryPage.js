16f13013198040e96617f782d970521ac0efc35c
Sullivan <adressecours0.1@gmail.com>
2021-05-17 15:48:57 +0200

    filter by type and language

--- client/src/components/LibraryPage.js
+++ client/src/components/LibraryPage.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react';
 // import sampleText from '../sampleText';
 import TextDiv from './TextDiv';
 import AddText from './AddText';
+import Select from 'react-select'
 
 export default class LibraryPage extends Component {
 
   state = {
     texts: [],
     name: '',
-    author: ''
+    author: '',
+    targetLang: '',//set the state manually
+    type: ''
   }
 
   componentDidMount = () => {
@@ -39,25 +42,63 @@ export default class LibraryPage extends Component {
     })
   }
 
-  render() {
-
-    let filterredTexts = this.state.texts.filter(e => {
-      return (`${e.title.toLowerCase()} ${e.author.toLowerCase()}`.includes(this.state.name.toLowerCase()))
+  updateTargetLang = e => {
+    console.log(e)
+    this.setState({
+      targetLang : e.value
     })
+  }
 
+  updateTargettype = e => {
+    this.setState({
+      type : e.value
+    })
+  }
+
+  render() {
+    
+    let filterredTexts = this.state.texts.filter(element => {
+      return (`${element.title.toLowerCase()} ${element.author.toLowerCase()}`.includes(this.state.name.toLowerCase()))
+      && (element.sourceLang === this.state.targetLang || !this.state.targetLang)
+      && (element.type === this.state.type || !this.state.type)
+
+     
+    })
+    console.log(filterredTexts.length)
     let mappedTexts = filterredTexts.map(text => <TextDiv key={ text._id } text={ text } />)
+
+    const langOptions = [
+      {value: '', label : 'All languages'},
+      {value : 'FR', label : 'French'},
+      {value : 'GER', label : 'German'},
+      {value : 'EN', label : 'English'},
+      {value : 'ES', label : 'Spanish'}
+    ]
+
+    const typeOptions = [
+      {value: '', label : 'All type'},
+      {value: 'book', label : 'Book'},
+      {value: 'poem', label : 'Poem'},
+      {value: 'article', label : 'Article'}
+    ]
 
     return (
       <main className="mx-auto p-10 text-center">
         <h1 className="text-3xl py-7">Library Page</h1>
+        <div>
         <form>
           <label>
             Title / Author:
-        </label>
+          </label>
           <input type="text"
             value={ this.state.name }
-            onChange={ this.handleQueryChange } />
+            onChange={ this.handleQueryChange } /><br />
+            <label htmlFor="targetLangSelect">Choose a language</label>
+            <Select id='targetLangSelect' options={langOptions} onChange={this.updateTargetLang}/><br />
+            <label htmlFor="typeSelect">Choose a type</label>
+            <Select id='typeSelect' options={typeOptions} onChange={this.updateTargettype}/>
         </form>
+        </div>
         {this.props.user && <AddText getText={ this.getAllTexts } /> }
         <div className="flex flex-wrap space-around ">
           { mappedTexts }
