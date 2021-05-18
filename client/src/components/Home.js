6ff8f37f092d212e58fd904805af6e64df79ccb4
Andy <andrew.mcginn@gmail.com>
2021-05-18 12:09:02 +0100

    some content and styling added to landing page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import React from 'react';
+import courseImg from "../images/courses__language__speak.jpg";
+import dictionaryImg from "../images/dictionaryVector.jpg";
+import poetryImg from "../images/free-poem-vector.jpg";
+import HomePageServiceDiv from './HomePageServiceDiv';
 
 class Home extends React.Component {
 
@@ -8,8 +11,17 @@ class Home extends React.Component {
   render() {
     return (
       <main>
-        <h1>Headline</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, ducimus?</p>
+        <section id="landing" className="flex flex-col items-center my-4">
+          <h1 className="text-white text-6xl my-6">LingoFish</h1>
+          <p className=" text-white text-3xl ">Language learning made authentic</p>
+ 
+        </section>
+        <section id="infoSection" className="flex flex-wrap justify-around p-10">
+          <HomePageServiceDiv heading="Instant translations" text="Just click on any word to see its translation in your chosen language" img={courseImg} />
+          <HomePageServiceDiv text="Add words to your vocab lists, use flashcards and view lists from other users." heading="Create your own lists and dictionary" img={dictionaryImg} />
+          <HomePageServiceDiv heading="Authentic, interesting texts" text="Choose from our library of articles, books and poems, or upload your favourite texts" img={poetryImg} />
+          
+        </section>
       </main>
     )
   }
