fe6a1e46f4904e2ff47a5c34326cdadbd7a12450
Sullivan <adressecours0.1@gmail.com>
2021-05-20 18:30:33 +0200

    last bit of styling for vocabulary

--- client/src/components/VocabListDetail.js
+++ client/src/components/VocabListDetail.js
@@ -27,15 +27,15 @@ export default class VocabListDetail extends Component {
     return (
       <div className="min-h-full bg-white">
       <div className="text-center">
-        <h1 className="font-bold text-2xl text-center mb-5 pt-10 text-yellow-500">{this.state.vocabListName}</h1>
+        <h1 className="font-bold text-4xl text-center mb-5 pt-10 text-yellow-500">{this.state.vocabListName}</h1>
         {this.state.vocabListWords.map((wordpairs, index) => {
           return (
-            <div key={index} className="text-lg leading-releaxed">
+            <div key={index} className="py-3 mx-auto w-1/3 rounded-md border-2 mb-3 text-3xl">
             <span className="text-blue-400">{wordpairs[0]} – </span><span className="text-green-600">{wordpairs[1]}</span>
             </div>
           )
         })}
-       <Link to={`/vocablist/${this.props.match.params.id}/flashcards`} > <button className="bg-green-600 hover:bg-yellow-500 duration-150 text-white px-3 py-2 rounded-lg mt-5">Learn this set!</button></Link>
+       <Link to={`/vocablist/${this.props.match.params.id}/flashcards`} > <button className="bg-green-600 hover:bg-yellow-500 duration-150 text-white px-3 py-2 rounded-lg mt-5 text-2xl">Learn this set!</button></Link>
       </div>
       </div>
     )
